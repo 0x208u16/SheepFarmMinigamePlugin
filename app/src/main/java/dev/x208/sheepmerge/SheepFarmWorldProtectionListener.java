c2cea5bf662556d105c7aec9809c2ec546d443f7
0x208u16 <enesmahya39@gmail.com>
2026-07-22 11:35:00 +0300

    Disabled block destruction in farm worlds.

--- app/src/main/java/dev/x208/sheepmerge/SheepFarmWorldProtectionListener.java
+++ app/src/main/java/dev/x208/sheepmerge/SheepFarmWorldProtectionListener.java
@@ -5,7 +5,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class SheepFarmWorldProtectionListener implements Listener {
 
@@ -39,7 +44,55 @@ public class SheepFarmWorldProtectionListener implements Listener {
         }
     }
 
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockBurn(BlockBurnEvent event) {
+        World world = event.getBlock() == null ? null : event.getBlock().getWorld();
+        if (isProtectedWorld(world)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockIgnite(BlockIgniteEvent event) {
+        World world = event.getBlock() == null ? null : event.getBlock().getWorld();
+        if (isProtectedWorld(world)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockSpread(BlockSpreadEvent event) {
+        World world = event.getBlock() == null ? null : event.getBlock().getWorld();
+        if (isProtectedWorld(world)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockExplode(BlockExplodeEvent event) {
+        World world = event.getBlock() == null ? null : event.getBlock().getWorld();
+        if (!isProtectedWorld(world)) {
+            return;
+        }
+        event.blockList().clear();
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityExplode(EntityExplodeEvent event) {
+        World world = event.getLocation() == null ? null : event.getLocation().getWorld();
+        if (!isProtectedWorld(world)) {
+            return;
+        }
+        event.blockList().clear();
+    }
+
     private boolean isPersonalWorld(World world) {
         return SheepMergeManager.isSheepFarmWorld(world);
+    }
+
+    private boolean isProtectedWorld(World world) {
+        return SheepMergeManager.isFarmBuildWorld(world)
+                || SheepMergeManager.isSheepFarmWorld(world)
+                || SheepMergeManager.isTutorialWorld(world);
     }
 }
